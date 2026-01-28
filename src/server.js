52626ec671e132e2048b46910f270a1f8f863631
alejoorlandi <alejo.orlandi57@gmail.com>
2026-01-28 19:44:45 -0300

    Integrate authentication and notes routes

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import notesRoutes from "./routes/notesRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 
@@ -7,6 +9,8 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use("/api/notes", notesRoutes);
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 3001;
 
